148796805d1e81a1c8776f5476402f6ff9731570
armin-fx <rechner_x@yahoo.de>
2016-05-06 19:36:20 +0200

    revert some changes

--- Marlin/UltiLCD2_menu_maintenance.cpp
+++ Marlin/UltiLCD2_menu_maintenance.cpp
@@ -534,7 +534,7 @@ static void lcd_menu_advanced_stats()
 {
     lcd_info_screen(NULL, lcd_change_to_previous_menu, MSGP_MENU_RETURN);
     lcd_lib_draw_string_centerP(10, PSTR("Machine on for:"));
-    char buffer[21] = {0};
+    char buffer[24] = {0};
     char* c = int_to_string(lifetime_minutes / 60, buffer, PSTR(":"));
     if (lifetime_minutes % 60 < 10)
         *c++ = '0';
